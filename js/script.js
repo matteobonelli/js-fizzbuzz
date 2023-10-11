94fc79ec81eb10fd3bf08f081801696fd9ab1417
matteobonelli <matteobonelli08@gmail.com>
2023-10-11 14:54:33 +0200

    js for cycle completed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-
+let text = ''
 
 for(let i = 1; i <= 100; i++){
-    console.log(i);
+    if(i % 15 === 0){
+        const fizzBuzz = 'FizzBuzz';
+        text = fizzBuzz;
+        console.log(text);
+    }
+    else if(i % 3 === 0){
+        const fizz = 'Fizz';
+        text = fizz;
+        console.log(text);
+    }
+    else if(i % 5 === 0){
+        const buzz = 'Buzz';
+        text = buzz;
+        console.log(text);
+    }
+    else{
+        console.log(i);
+    }
 }
+

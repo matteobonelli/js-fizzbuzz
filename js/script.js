634d95d1d9c18d5c296cfb8a929cde88a6a174fb
matteobonelli <matteobonelli08@gmail.com>
2023-10-11 15:25:47 +0200

    now different colors squares are visualized in html

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
+const myContainer = document.getElementById('container');
+
 let text = ''
 
 for(let i = 1; i <= 100; i++){
+    let bgBox;
     if(i % 15 === 0){
         const fizzBuzz = 'FizzBuzz';
         text = fizzBuzz;
+        bgBox = 'bg-danger';
         console.log(text);
     }
     else if(i % 3 === 0){
         const fizz = 'Fizz';
         text = fizz;
+        bgBox = 'bg-success';
         console.log(text);
     }
     else if(i % 5 === 0){
         const buzz = 'Buzz';
         text = buzz;
+        bgBox = 'bg-warning';
         console.log(text);
     }
     else{
         console.log(i);
+        bgBox = 'my-bg-box';
     }
+    const boxEl = document.createElement('div');
+    boxEl.className = `box ${bgBox} d-flex justify-content-center align-items-center`;
+    myContainer.append(boxEl);
 }
 
